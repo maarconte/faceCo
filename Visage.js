4dadd9ecad0239178d16d2364da16676729221b9
Thomas Millet <thomas@mbp-de-thomas.edu.sir>
2017-11-29 15:53:12 +0100

    add face

---
+++ Visage.js
@@ -0,0 +1,30 @@
+function Face(forme)
+{
+    this.face = true;
+    if (forme == /*string*/)
+    {
+        return true;
+    }
+    return false;
+}
+
+function Eyes(formeYeux)
+{
+    this.eyes = true;
+    if (eyes == /*string*/)
+    {
+        return true;
+    }
+    return false;
+}
+
+function colorHair(couleurCheveux)
+{
+    this.colorHair = true;
+    if (colorHair == /*string*/)
+    {
+        return true;
+    }
+    return false;
+}
+
